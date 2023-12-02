2b4428554c2e9986e21dfab319317ac0b2920bec
llt <liuliteng@163.com>
2023-12-02 15:31:29 +0800

    first commit

---
+++ robot_voice_ws/build/robot_voice/CMakeFiles/voice_node.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voice_node.dir/src/demo.cpp.o"
+  "CMakeFiles/voice_node.dir/src/linuxrec.c.o"
+  "CMakeFiles/voice_node.dir/src/speech_recognizer.c.o"
+  "voice_node"
+  "voice_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/voice_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
